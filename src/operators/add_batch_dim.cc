d82180a098a9344d60d28ce39c22326790e92e85
Aditya Ajay <aditya.ajay33@gmail.com>
2025-11-22 17:24:49 -0800

    testing pushing

--- src/operators/add_batch_dim.cc
+++ src/operators/add_batch_dim.cc
@@ -7,6 +7,7 @@ namespace ptk
     namespace operators
     {
         // TODO: Implement AddBatchDim
+        // This function should add a batch dimension to the input tensor.
         core::Status AddBatchDim(const data::TensorView &src, data::TensorView *dst)
         {
             return core::Status(core::StatusCode::kInternal, "AddBatchDim not yet implemented");
